de514ce4148e535dccf8852a65a048d34c16c7da
Martin Simon <martin@tentwentyfour.lu>
2017-05-11 14:20:42 +0200

    v.0.1

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
 # puppet-rinetd
+
+Install and manage rinetd(8) via Puppet.
+
+## Usage
+
+### Install rinetd with default config
+
+```puppet
+    class { 'rinetd': }
+```
+
+### Set allow and deny rules
+
+```puppet
+class { 'rinetd':
+    allow => ['192.168.178.1', '10.24.0.1', '10.24.42.1'],
+    deny => ['192.168.1.*'],
+}
+```
+
+#### Using hiera
+
+```puppet
+rinetd::allow:
+  - '192.168.178.1'
+  - '10.24.0.1'
+  - '10.24.42.1'
+
+rinetd::deny:
+  - '192.168.1.*'
+```
+
+### Set forwarding rules
+
+```puppet
+class { 'rinetd':
+    rules => [
+        '192.168.178.1 8080 10.24.0.1 443',
+        '10.24.42.1 5901 192.168.7.49 3456',
+    ],
+}
+```
+
+#### Using hiera
+
+```puppet
+rinetd::rules:
+  - '192.168.178.1 8080 10.24.0.1 443'
+  - '10.24.42.1 5901 192.168.7.49 3456'
+```
+
+### Set logfile path
+
+```puppet
+class { 'rinetd':
+    logfile => ['/var/log/example.log'],
+}
+```
+
+#### Using hiera
+
+```puppet
+rinetd::logfile: '/var/log/example.log'
+```
+
+### Use web-server style logfile format
+
+```puppet
+class { 'rinetd':
+    logcommon => true,
+}
+```
+
+#### Using hiera
+
+```puppet
+rinetd::logcommon: true
+```
+
+## rinetd class parameters
+
+| Parameter           | Type    | Default             | Description |
+| :-------------------| :------ |:------------------- | :---------- |
+| allow               | array   | []                  | set allow rules |
+| autoupgrade         | boolean | false               | ugrade package automatically if there is a newer version |
+| deny                | array   | []                  | set deny rules |
+| rules               | array   | []                  | set forwarding rules |
+| logfile             | string  | /var/log/rinetd.log | set logfile path |
+| logcommon           | boolean | false               | use web-server style logfile format |
+| ensure              | string  | present             | latest,present or absent |
+| service_manage      | boolean | true                | manage rinetd service state |
+| service_restart     | boolean | undef               | manage service restart |
+
+## License:
+
+```
+Apache v2.0 License
+Copyright 2017 Martin Simon
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ manifests/init.pp
@@ -0,0 +1,119 @@
+# == Class: rinetd
+#
+# This class initializes the rinetd(8) class
+#
+# Parameters:
+#   [*allow*]
+#     Set global allow rules.
+#     Only IP addresses are matched, hostnames cannot be specified.
+#     Possible wildcards are * and ?
+#     Default: []
+#
+#   [*autoupgrade*]
+#     Upgrade package automatically, if there is a newer version.
+#     Default: false
+#
+#   [*deny*]
+#     Set global deny rules.
+#     Only IP addresses are matched, hostnames cannot be specified.
+#     Possible wildcards are * and ?
+#     Default: []
+#
+#   [*rules*]
+#     Set forwarding rules.
+#     Both IP addresses and hostnames are accepted for bindaddress and connectaddress.
+#     Default: []
+#
+#   [*logfile*]
+#     Set logfile path.
+#     Default: /var/log/rinetd.log
+#
+#   [*logcommon*]
+#     Activate web server-style "common log format" logging.
+#     Default: false
+#
+#   [*ensure*]
+#     Ensure if present or absent.
+#     Default: present
+#
+#   [*service_manage*]
+#     Whether service state should be managed by Puppet.
+#     Default: true
+#
+#   [*service_restart*]
+#     Force service restart after configuration file is modified.
+#     Default: true
+#
+# Actions:
+#   Installs and configures rinetd(8).
+#
+# Sample Usage:
+#   See README
+#
+class rinetd(
+    $allow           = [],
+    $autoupgrade     = false,
+    $deny            = [],
+    $rules           = [],
+    $logfile         = '/var/log/rinetd.log',
+    $logcommon       = false,
+    $ensure          = 'present',
+    $service_manage  = true,
+    $service_restart = true,
+) {
+
+    validate_bool($autoupgrade)
+    validate_bool($logcommon)
+    validate_bool($service_manage)
+    validate_bool($service_restart)
+
+    case $ensure {
+        /(present)/: {
+            if $autoupgrade == true {
+                $package_ensure = 'latest'
+            } else {
+                $package_ensure = 'present'
+            }
+            $service_ensure = 'running'
+            $service_enable = true
+        }
+        /(absent)/: {
+            $package_ensure = 'absent'
+            $service_ensure = 'stopped'
+            $service_enable = false
+        }
+        default: {
+            fail('ensure parameter must be present or absent')
+        }
+    }
+
+    package { 'rinetd':
+        ensure   => $package_ensure,
+    }
+
+    if $service_restart and $service_manage {
+        $service_notify_real = Service['rinetd']
+    } else {
+        $service_notify_real = undef
+    }
+
+    if $service_manage {
+        service { 'rinetd':
+            ensure     => $service_ensure,
+            enable     => $service_enable,
+            hasrestart => true,
+            require => Package['rinetd']
+        }
+    }
+
+    file { 'rinetd_conf':
+        ensure => file,
+        owner => 'root',
+        group => 'root',
+        mode    => '0644',
+        path => '/etc/rinetd.conf',
+        content => template('rinetd/rinetd.conf.erb'),
+        require => Package['rinetd'],
+        notify  => $service_notify_real,
+    }
+}
