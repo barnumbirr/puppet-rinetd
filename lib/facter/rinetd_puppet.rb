46d1fb400ddcbda4bb762f8c5cb0b94c0473f400
Martin Simon <me@martinsimon.me>
2019-11-15 17:23:24 +0100

    fix Github action, fix linting on custom fact

--- lib/facter/rinetd_puppet.rb
+++ lib/facter/rinetd_puppet.rb
@@ -1,15 +1,15 @@
 require 'puppet'
 
 Facter.add(:rinetd_version) do
-    setcode do
-        osfamily = Facter.value('osfamily')
-        case osfamily
-        when 'Debian'
-            command = '/usr/bin/dpkg-query -f \'${Status}\t${Version}\n\' -W rinetd > /dev/null'
-            version = Facter::Util::Resolution.exec(command)
-            if version =~ %r{.*[install|hold] ok installed;([^;]+);.*}
-                Regexp.last_match(1)
-            end
-        end
+  setcode do
+    osfamily = Facter.value('osfamily')
+    case osfamily
+    when 'Debian'
+      command = '/usr/bin/dpkg-query -f \'${Status}\t${Version}\n\' -W rinetd > /dev/null'
+      version = Facter::Util::Resolution.exec(command)
+      if version =~ %r{.*[install|hold] ok installed;([^;]+);.*}
+        Regexp.last_match(1)
+      end
     end
+  end
 end
